3356354896bb72c08487e38f3b6c985ae09b07f9
Travis Raines <571832+rainest@users.noreply.github.com>
2022-04-15 12:09:27 -0700

    api: add RouteReasonNotAllowedByListeners
    Add a RouteConditionReason constant, "NotAllowedByListeners", for use
    when a route fails to match any of the allowedRoutes conditions on a
    Gateway's Listeners.

--- apis/v1alpha2/shared_types.go
+++ apis/v1alpha2/shared_types.go
@@ -197,6 +197,10 @@ const (
 	//
 	// * "Accepted"
 	//
+	// Possible reasons for this condition to be False are:
+	//
+	// * "NotAllowedByListeners"
+	//
 	// Controllers may raise this condition with other reasons,
 	// but should prefer to use the reasons listed above to improve
 	// interoperability.
@@ -227,6 +231,11 @@ const (
 	// This reason is used with the "Accepted" condition when the Route has been
 	// accepted by the Gateway.
 	RouteReasonAccepted RouteConditionReason = "Accepted"
+
+	// This reason is used with the "Accepted" condition when the route has not
+	// been accepted by a Gateway because the Gateway had no Listener whose
+	// allowedRoutes criteria permit the route
+	RouteReasonNotAllowedByListeners RouteConditionReason = "NotAllowedByListeners"
 
 	// This reason is used with the "ResolvedRefs" condition when the condition
 	// is true.
